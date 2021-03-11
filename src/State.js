fcdbb1a2bd09b7ac071e41a78ca35e2715184ccb
Rydetery <rydetery1@gmail.com>
2021-03-11 16:46:50 +0300

    0.37 Refactoring State.js file to campare OOP obj 'store'

--- src/State.js
+++ src/State.js
@@ -1,63 +1,67 @@
-let renderEntireTree = () => {
-    console.log('State was changed');
+let store = {
+    _state: {
+        dialogsData : [
+            {id: 1, name: 'Dmitry'},
+            {id: 2, name: 'Nikolay'},
+            {id: 3, name: 'Valery'},
+            {id: 4, name: 'Svetlana'},
+            {id: 5, name: 'Ignat'},
+            {id: 6, name: 'Semen'}
+        ],
+        profilePage: {
+            posts : [
+                {id: 1, message: 'I am the Volan de Mort fan', likes: 6},
+                {id: 2, message: 'I should never say his name!', likes: 13},
+                {id: 3, message: 'However, now I am a Yoda from Star Wars fan', likes: 4}
+                ],
+            newPostText: 'Type something'
+            },
+        messagesData : [
+              {id: 1, message: 'Hello there!'},
+              {id: 2, message: 'General Kenobi?!'}
+        ]
+    },
+    getState () {
+        return this._state
+    },
+    callSubscriber () {
+        console.log('State was changed');
+    },
+    addDialogsData (name) {
+        let user = {
+            id: this._state.dialogsData[this._state.dialogsData.length],
+            name: name
+        }
+        this._state.dialogsData.push(user);
+        this.callSubscriber(this._state);
+    },
+    addPost () {
+        let post = {
+            id: this._state.profilePage.posts[this._state.profilePage.posts.length],
+            message: this._state.profilePage.newPostText,
+            likes: 0
+        }
+        this._state.profilePage.posts.push(post);
+        this._state.profilePage.newPostText = '';
+        this.callSubscriber(this._state);
+    },
+    updateText (text) {
+        this._state.profilePage.newPostText = text;
+        this.callSubscriber(this._state);
+    },
+    addMessage (message) {
+        let newMessage = {
+            id: this._state.messagesData[this._state.messagesData.length],
+            message: message
+        }
+        this._state.messagesData.push(newMessage)
+        this.callSubscriber(this._state)
+    },
+    subscribe (observer) {
+        this.callSubscriber = observer;
+    }
 }
 
-let state = {
-    dialogsData : [
-        {id: 1, name: 'Dmitry'},
-        {id: 2, name: 'Nikolay'},
-        {id: 3, name: 'Valery'},
-        {id: 4, name: 'Svetlana'},
-        {id: 5, name: 'Ignat'},
-        {id: 6, name: 'Semen'}
-    ],
-    profilePage: {
-        posts : [
-            {id: 1, message: 'I am the Volan de Mort fan', likes: 6},
-            {id: 2, message: 'I should never say his name!', likes: 13},
-            {id: 3, message: 'However, now I am a Yoda from Star Wars fan', likes: 4}
-            ],
-        newPostText: 'Type something'
-        },
-    messagesData : [
-          {id: 1, message: 'Hello there!'},
-          {id: 2, message: 'General Kenobi?!'}
-    ]
-};
-window.state = state;
-export const addDialogsData = (name) => {
-    let user = {
-        id: state.dialogsData[state.dialogsData.length],
-        name: name
-    }
-    state.dialogsData.push(user);
-    renderEntireTree();
-}
-export const addPost = () => {
-    let post = {
-        id: state.profilePage.posts[state.profilePage.posts.length],
-        message: state.profilePage.newPostText,
-        likes: 0
-    }
-    state.profilePage.posts.push(post);
-    state.profilePage.newPostText = '';
-    renderEntireTree();
+window.store = store;
 
-}
-export const updateText = (text) => {
-    state.profilePage.newPostText = text;
-    renderEntireTree();
-}
-export const addMessage = (message) => {
-    let newMessage = {
-        id: state.messagesData[state.messagesData.length],
-        message: message
-    }
-    state.messagesData.push(newMessage)
-    renderEntireTree()
-}
-
-export const subscribe = (observer) => {
-    renderEntireTree = observer;
-}
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import state, { subscribe, updateText, addPost, addMessage } from './State';
+import store from './State';
 
-let renderEntireTree = () => {
+let renderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-          <App state={state} addPost={addPost} addMessage={addMessage} updateText= {updateText} />
+          <App state={state} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} updateText= {store.updateText.bind(store)} />
         </React.StrictMode>,
         document.getElementById('root')
       );
   }
-renderEntireTree();
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
